06e31eb6e5f5ef87aeb6324dc23549cb76359c03
Zoe Fisher <zrfisher@usc.edu>
2022-01-25 10:44:25 -0800

    Initial Commit

---
+++ Shared/haptics_test_platformApp.swift
@@ -0,0 +1,17 @@
+//
+//  haptics_test_platformApp.swift
+//  Shared
+//
+//  Created by Zoe Fisher on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct haptics_test_platformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
